031004ab68431e7f07195d436d12eb98ad5ba897
Shoaib Arif <shoaib.techxcape@gmail.com>
2022-10-20 03:02:55 +0500

    feature/ added suspense for home

--- components/common/loader/page-loader.js
+++ components/common/loader/page-loader.js
@@ -7,7 +7,7 @@ const PageLoader = () => {
                 <div className="absolute left-[calc(50% - 4em)] top-[calc(50% - 4em)] transition-opacity z-50">
                     <div
                         className="animate-spin border-2 border-[rgba(0, 0, 0, 0.2)] border-l-[17.6px] border-l-primary__color w-[10em] h-[10em] rounded-full"/>
-                    <div className="z-50 bg-[url('/assets/logo.png')] bg-contain bg-center bg-no-repeat h-full w-[50%] absolute top-0 translate-x-[50%] translate-y-0"/>
+                    <div className="z-50 bg-[url('../assets/logo.png')] bg-contain bg-center bg-no-repeat h-full w-[50%] absolute top-0 translate-x-[50%] translate-y-0"/>
                 </div>
             </div>
         </>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,36 +1,65 @@
-import Navbar from '../components/navbar'
-import TopNavbar from '../components/topnavbar'
-import HeroPage from '../components/hero'
-import SearchForm from '../components/searchform'
-import AboutUs from '../components/aboutus'
-import Categories from '../components/categories'
-import DownloadApp from '../components/downloadapp'
-import ClinicsSection from '../components/clinicssection'
-import LabSection from '../components/labsection'
-import Doctor from "../components/doctor";
-import BlogList from "../components/blog-list";
-import Subscribe from "../components/subscribe";
-import Footer from "../components/footer/footer";
+import dynamic from 'next/dynamic'
+import { Suspense } from 'react'
+import PageLoader from "../components/common/loader/page-loader";
 
-// imagesimports
-
+const Navbar = dynamic(() => import('../components/navbar'), {
+    suspense: true,
+})
+const TopNavbar = dynamic(() => import('../components/topnavbar'), {
+    suspense: true,
+})
+const HeroPage = dynamic(() => import('../components/hero'), {
+    suspense: true,
+})
+const SearchForm = dynamic(() => import('../components/searchform'), {
+    suspense: true,
+})
+const AboutUs = dynamic(() => import('../components/aboutus'), {
+    suspense: true,
+})
+const Categories = dynamic(() => import('../components/categories'), {
+    suspense: true,
+})
+const ClinicsSection = dynamic(() => import('../components/clinicssection'), {
+    suspense: true,
+})
+const LabSection = dynamic(() => import('../components/labsection'), {
+    suspense: true,
+})
+const BlogList = dynamic(() => import('../components/blog-list'), {
+    suspense: true,
+})
+const Doctor = dynamic(() => import('../components/doctor'), {
+    suspense: true,
+})
+const Footer = dynamic(() => import('../components/footer/footer'), {
+    suspense: true,
+})
+const Subscribe = dynamic(() => import('../components/subscribe'), {
+    suspense: true,
+})
+const DownloadApp = dynamic(() => import('../components/downloadapp'), {
+    suspense: true,
+})
 
 export default function Home() {
   return (
     <>
-      <TopNavbar/>
-      <Navbar/>
-        <HeroPage/>
-        <SearchForm/>
-      <AboutUs/>
-      <Categories/>
-      <DownloadApp/>
-      <ClinicsSection/>
-      <LabSection/>
-        <Doctor/>
-        <BlogList/>
-        <Subscribe/>
-        <Footer/>
+        <Suspense fallback={ <PageLoader/>}>
+            <TopNavbar/>
+            <Navbar/>
+              <HeroPage/>
+              <SearchForm/>
+            <AboutUs/>
+            <Categories/>
+            <DownloadApp/>
+            <ClinicsSection/>
+            <LabSection/>
+              <Doctor/>
+              <BlogList/>
+              <Subscribe/>
+              <Footer/>
+        </Suspense>
     </>
     // <div className='w-full  bg-blue-500 flex justify-center  items-center '>
     //   <div className='h-1/2 w-1/2 bg-amber-500 flex justify-center items-center'>
